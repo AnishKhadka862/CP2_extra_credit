2f23d5d47f0e36dc28270c65556bb7bec0587108
Anish Khadka <akhadka2@sfsu.edu>
2023-04-13 11:40:25 -0700

    Update benchmark.cpp

--- benchmark.cpp
+++ benchmark.cpp
@@ -6,6 +6,17 @@
 // usage: no command line arguments
 // set problem sizes, block sizes in the code below
 
+//
+// (C) 2022, E. Wes Bethel
+// benchmark-* harness for running different versions of the sum study
+//    over different problem sizes
+//
+// usage: no command line arguments
+// set problem sizes, block sizes in the code below
+
+//Anish Khadka
+//CSC 656 extra credit Coding Project 
+
 #include <algorithm>
 #include <chrono>
 #include <iomanip>
@@ -14,41 +25,53 @@
 #include <vector>
 #include <string.h>
 
-
 extern void setup(int64_t N, uint64_t A[]);
 extern int64_t sum(int64_t N, uint64_t A[]);
 
-/* The benchmarking program */
-int main(int argc, char** argv) 
-{
-   std::cout << std::fixed << std::setprecision(2);
+int main(int argc, char** argv) {
+  std::cout << std::fixed << std::setprecision(5);
 
-#define MAX_PROBLEM_SIZE 1 << 28  //  256M
-   std::vector<int64_t> problem_sizes{ MAX_PROBLEM_SIZE >> 5, MAX_PROBLEM_SIZE >> 4, MAX_PROBLEM_SIZE >> 3, MAX_PROBLEM_SIZE >> 2, MAX_PROBLEM_SIZE >> 1, MAX_PROBLEM_SIZE};
-   
-   std::vector<uint64_t> A(MAX_PROBLEM_SIZE);
+  const int64_t N_SMALL = 1000000;
+  const int64_t N_LARGE = 256000000;
 
-   int64_t t;
-   int n_problems = problem_sizes.size();
+  std::vector<int64_t> problem_sizes{N_SMALL, N_LARGE};
 
-   /* For each test size */
-   for (int64_t n : problem_sizes) 
-   {
-      printf("Working on problem size N=%d \n", n);
+  std::vector<uint64_t> A(N_LARGE);
 
+  int64_t t;
+  int n_problems = problem_sizes.size();
+
+  // Run for at least 30 seconds
+  double elapsed_time = 0.0;
+  int64_t num_sum_calls = 0;
+  auto start_time = std::chrono::high_resolution_clock::now();
+  while (elapsed_time < 30.0) {
+    for (int64_t n : problem_sizes) {
       // invoke user code to set up the problem
       setup(n, &A[0]);
 
-      // insert your timer code here
-
       // invoke method to perform the sum
       t = sum(n, &A[0]);
+      num_sum_calls++;
 
-      // insert your end timer code here, and print out elapsed time for this problem size
+      if (num_sum_calls % 10 == 0) {
+        auto end_time = std::chrono::high_resolution_clock::now();
+        elapsed_time = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time).count();
+        if (elapsed_time >= 30.0) {
+          break;
+        }
+      }
+    }
+  }
 
-      printf(" Sum result = %lld \n",t);
-
-   } // end loop over problem sizes
+  // Print results
+  std::cout << "Total sum calls: " << num_sum_calls << std::endl;
+  std::cout << "Elapsed time: " << elapsed_time << " s" << std::endl;
+  std::cout << "Number of times each problem size was executed: ";
+  for (int64_t n : problem_sizes) {
+    int64_t num_executions = num_sum_calls / (n == N_SMALL ? 1 : 256);
+    std::cout << num_executions << " ";
+  }
+  std::cout << std::endl;
 }
 
-// EOF
